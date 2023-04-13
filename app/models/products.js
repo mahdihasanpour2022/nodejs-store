876a7b621694168f50f72043a1bc48e1bcd85582
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-04-13 14:04:04 -0700

    Fix : Edit  product schema model

--- app/models/products.js
+++ app/models/products.js
@@ -1,24 +1,24 @@
 // step 13 :
 const { default: mongoose } = require("mongoose");
+const { CommentSchema } = require("./public.schema");
 
 const Schema = new mongoose.Schema({
   title: { type: String, require: true },
-  short_desc: { type: String, require: true },
-  tottal_desc: { type: String, require: true },
+  short_text: { type: String, require: true },
+  text: { type: String, require: true },
   images: { type: [String], require: true },
   tags: { type: [String], default: [] },
-  category: { type: mongoose.Types.ObjectId, require: true },
-  comments: { type: [], default: []  },
-  like: { type: [mongoose.Types.ObjectId], default: []  },
-  deslike: { type: [mongoose.Types.ObjectId], default: []  },
-  bookmark: { type: [mongoose.Types.ObjectId], default: [] },
+  category: { type: mongoose.Types.ObjectId,ref : "category" , require: true },
+  comments: { type: [CommentSchema], default: []  },
+  likes: { type: [mongoose.Types.ObjectId], default: []  },
+  dislikes: { type: [mongoose.Types.ObjectId], default: []  },
+  bookmarks: { type: [mongoose.Types.ObjectId], default: [] },
   price: { type: Number, default: 0  },
   discount: { type: Number, default: 0 },
   count: { type: Number },
-  type: { type: String, require: true },
-  time: { type: String },
+  type: { type: String, require: true }, // vrtual - phisicak product
   format: { type: String },
-  teacher: { type: mongoose.Types.ObjectId, require: true },
+  supplier: { type: mongoose.Types.ObjectId, require: true },
   feature: {
     type: Object,
     default: {
