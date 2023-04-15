96c51f0bb81f5a6bd8d165a7ce095a6cdbaf9f40
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-04-15 09:47:59 -0700

    Feat : create function  findProduct function for find product with id in db

---
+++ app/http/validators/public.validator.js
@@ -0,0 +1,11 @@
+//  step 137 : validate har jaie mitone estefade beshe chon public hast
+const Joi = require("@hapi/joi");
+const { MONGO_ID_PATTERN } = require("../../utils/constants");
+const createHttpError = require("http-errors");
+
+//آبجکت حاوی مقادیری مثل آیدی  رو میدیم به جویی ولیدیت میکنه میریزه تو این 
+const ObjectValidator = Joi.object({
+  id : Joi.string().pattern(MONGO_ID_PATTERN).error(new Error(createHttpError.BadRequest("ایدی شناسه وارد شده صحیح نیست")))
+});
+
+module.exports = {ObjectValidator};
