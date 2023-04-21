941356a57a25e538a653fa36c1a7eb62187a4c72
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-04-21 10:20:00 -0700

    Fix: Add otp generator packaje

--- app/utils/randomNumberGenerator.js
+++ app/utils/randomNumberGenerator.js
@@ -1,8 +1,18 @@
 // step 25 :
+const otpGenerator = require("otp-generator");
+
 function randomNumberGenerator() {
-  return +Math.floor(Math.random() * 90000).toString().slice(0, 5);
-};
- 
-module.exports = {
-  randomNumberGenerator
+  // way1 :
+  // return +Math.floor(Math.random() * 90000).toString().slice(0, 5);
+  // way 2 :
+  // sakhte otp ba packaje otpGenerator
+  return otpGenerator.generate(5, {
+    digits: true,
+    lowerCaseAlphabets: false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  });
 }
+module.exports = {
+  randomNumberGenerator,
+};
