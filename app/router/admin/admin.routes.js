39b4ce18be500c0443c17254945aa1479de00090
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-04-22 00:21:42 -0700

    Feat : cerate course router

--- app/router/admin/admin.routes.js
+++ app/router/admin/admin.routes.js
@@ -1,24 +1,27 @@
 const { Blog_AdminApiRoutes } = require("./blog");
 const { Category_AdminApiRoutes } = require("./category");
+const { Course_AdminApiRoutes } = require("./course");
 const { Product_AdminApiRoutes } = require("./product");
 
 const router = require("express").Router();
 
 // step 128 : add  Product(AdminPanel) here
-
+// step 154 :  add course(AdminPanel) here
 /**
  * @swagger
  * tags:
  *  -    name: admin-panel
  *       description : actions for admins (add , remove , edit , ...)
+ *  -    name: course(AdminPanel)
+ *       description : management course routes
  *  -    name: category(AdminPanel)
  *       description : all method for category section
  *  -    name: product(AdminPanel)
  *       description : management Product routes
- *  -    name: prisma(Api)
- *       description : create some api with prisma and postgreSQL category section
  *  -    name: blog(AdminPanel)
  *       description : made blog mangement admin panel
+ *  -    name: prisma(Api)
+ *       description : create some api with prisma and postgreSQL category section
  */
 
 // step 129 :
@@ -29,6 +32,9 @@ router.use("/category", Category_AdminApiRoutes);
 
 //step 62 :inja bashe vase hame route haie admin bshe azash estefade kone va baraie hamie routes ha tag ijad kon
 router.use("/blogs", Blog_AdminApiRoutes);
+
+// step 153 :
+router.use("/courses" , Course_AdminApiRoutes)
 
 module.exports = {
   adminRoutes: router,
---
+++ app/router/admin/course.js
@@ -0,0 +1,14 @@
+// step 152 : create route for courses
+const router = require("express").Router();
+
+// router.get(); // get all courses
+// router.get(); // get a courses
+// router.post(); // create new courses
+// router.put(); // create new chapter
+// router.put(); // create new episode
+// router.delete(); // remove a course
+// router.patch(); // edit a course
+
+module.exports = { 
+  Course_AdminApiRoutes: router
+ };
