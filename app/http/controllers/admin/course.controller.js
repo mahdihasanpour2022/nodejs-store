bb399d036445fd21f89b5f6e748626b93fc75466
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-04-22 00:50:08 -0700

    Feat : Add getAllCourses

--- app/http/controllers/admin/course.controller.js
+++ app/http/controllers/admin/course.controller.js
@@ -2,18 +2,27 @@
 
 const { CourseModel } = require("../../../models/course");
 const Controller = require("../controller");
+const { StatusCodes } = require("http-status-codes");
 
 class CourseController extends Controller {
   async getAllCourses(req, res, next) {
     try {
-      // const courses = await CourseModel.find({});
-      return res.json({});
+      const courses = await CourseModel.find({}).sort({ _id: -1 }); // sort ba id -1 iani akharin record aval neshon dade mishe
+      return res.status(StatusCodes.OK).json({
+        isSuccess: true,
+        statusCode: StatusCodes.OK,
+        message: "لیست تمام دوره ها با موفقیت گرفته شد.",
+        data: {
+          courses,
+        },
+        error: null,
+      });
     } catch (error) {
       next(error);
     }
   }
-};
+}
 
 module.exports = {
-  CourseController : new CourseController()
+  CourseController: new CourseController(),
 };
